9f418c42ad415d2f0177af5c35a5c8c496da8d6c
thomass238 <thomass238@nycstudents.net>
2026-04-23 10:17:13 -0400

    Update rowlet.py

--- rowlet.py
+++ rowlet.py
@@ -8,5 +8,14 @@ data = json.load(pokedex)
 
 # for index, item in enumerate(data):
 #     print(item["name"])
+
+# lang=input("What language do you prefer: english, japanese, chinese, or french?         ")
+# for d in data:
+#     print(d["name"][lang])
+
+
+user = input("Pick a Pokemon type: Normal, Fighting, Flying, Poison, Ground, Rock, Bug, Ghost, Steel, Fire, Water, Grass, Electric, Psychic, Ice, Dragon, Dark, Fairy")
+
 for d in data:
-    print(d["name"]["english"])
+    print(d["Ice"])
+
